b5b19f2d65701386b5020536d26b203c2c3ef8b8
Gaur1234567890 <gauravnamdev02000@outlook.com>
2024-01-25 11:31:39 +0530

    Break the number 4

---
+++ Break.java
@@ -0,0 +1,12 @@
+public class Break {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            if(i==4)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ Sum_of_first_And_last.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Sum_of_first_And_last {
+    public static void main(String[] args) {
+        int n,sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        n = sc.nextInt();
+        int r = n%10;
+        if(n<9)
+        {
+            sum=-1;
+        }
+        while(n>0)
+        {
+          
+          sum = sum+r;
+          n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
