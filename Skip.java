323190c1fcd17e7d480d4b3fdb3d8a4bffc958b0
Gaur1234567890 <gauravnamdev02000@outlook.com>
2024-01-25 11:49:11 +0530

    Skip the element

---
+++ Skip.java
@@ -0,0 +1,12 @@
+public class Skip {
+    public static void main(String[] args) {
+        for(int i=1 ;i<=10;i++)
+        {
+            if(i==4 || i==6)
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
