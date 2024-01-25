3a8804d7ba66a009fb8dcaf8a34dc3b7bf757f6c
Gaur1234567890 <gauravnamdev02000@outlook.com>
2024-01-25 11:18:14 +0530

    count the number

---
+++ Count.java
@@ -0,0 +1,15 @@
+import java. util.*;
+public class Count {
+    public static void main(String[] args) {
+        int n , count=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        n = sc.nextInt();
+        while(n>0)
+        {
+            count ++;
+            n= n/10;
+        }
+        System.out.println(count);
+    }
+}
