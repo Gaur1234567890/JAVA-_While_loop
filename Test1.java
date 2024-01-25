c97feaca8644e737664db87142b79a9bf3ec34a6
Gaur1234567890 <gauravnamdev02000@outlook.com>
2024-01-25 11:06:10 +0530

    reverse number 5 to 1

---
+++ Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public static void main(String [] args)
+    {
+        int a = 5;
+        while(a>=1)
+        {
+            System.out.println(a);
+            a--;
+        }
+    }
+}
